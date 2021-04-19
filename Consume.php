cd70e8e63181c1ce2684012b426f69533037dbd3
TranMinn <1801040141@s.hanu.edu.vn>
2021-04-20 00:31:36 +0700

    Reusable Code

---
+++ Consume.php
@@ -0,0 +1,36 @@
+<?php
+
+
+    function consume($url){
+        // Send request to resource
+        $client = curl_init($url);
+
+        // Set options
+        curl_setopt($client, CURLOPT_URL, $url);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+
+        // Get response 
+        $response = curl_exec($client);
+        curl_close($client);
+
+        // Decode response
+        $response = json_decode($response, true);
+
+        if(isset($response['status'])){
+            if($response['status'] == '200'){
+                $data = $response['data'];
+                return $data;
+            }else{
+                echo $response['data'];
+            }
+
+        }else{
+            echo "API failed!";
+        }
+
+        return $data;
+    }
+
+
+
+?>
---
+++ test_consume.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'Consume.php';
+
+    $url = "http://localhost:8088/myNews/api/article/read.php";
+
+    $data = consume($url);
+
+    echo count($data);
+
+    // $result = implode(' ', $data);
+
+    // print_r($data[0]);
+    for($i = 0; $i < count($data); $i++){
+?>
+
+        <div>
+            <img src="articleImages/<?php echo htmlentities($data[$i]['image']);?>" alt="<?php echo htmlentities($data[$i]['title']);?>">
+            <div>
+              <h2><?php echo htmlentities($data[$i]['title']);?></h2>
+                 <p><b>Author : </b><?php echo htmlentities($data[$i]['author']);?></a> </p>
+       
+              <a href="">Read More &rarr;</a>
+        </div>
+
+<?php } ?>
+
+<!-- <h2>Hello</h2> -->
