497d360a8305a9981c1550d86333908c5a52c60f
coang <1801040172@s.hanu.edu.vn>
2021-04-23 23:45:48 +0700

    add cate & tag

--- admin/add-tag.php
+++ admin/add-tag.php
@@ -1,1 +1,240 @@
-<!-- TO-DO -->
+<?php
+
+include '../consume.php';
+
+session_start();
+error_reporting(0);
+if(strlen($_SESSION['login'])==0)
+  { 
+    session_destroy();
+    header('location:index.php');
+}
+else{  
+
+    // Add new Cate
+    if(isset($_POST['submit'])){
+
+        $name = $_POST['name'];
+        
+        $form_data = array ('name' => $name);
+        
+    $api_url = "http://localhost:8088/myNews/api/tag/create.php";
+
+    $client = curl_init($api_url);
+    curl_setopt($client, CURLOPT_URL, $api_url);
+    curl_setopt($client, CURLOPT_POST, true);
+    curl_setopt($client, CURLOPT_POSTFIELDS, json_encode($form_data));
+    curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($client);
+
+    curl_close($client);
+
+    if($e = curl_error($client)){
+        $error = "Upload failed!!!";
+    }else{
+        $response = json_decode($response, true);
+        echo "<script type='text/javascript'> window.location.href = 'manage-tags.php'; </script>";
+    }
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <!-- App title -->
+        <title>AZ News | Add Category</title>
+
+        <!-- Summernote css -->
+        <link href="../plugins/summernote/summernote.css" rel="stylesheet" />
+
+        <!-- Select2 -->
+        <link href="../plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- Jquery filer css -->
+        <link href="../plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
+        <link href="../plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
+        <script src="assets/js/modernizr.min.js"></script>
+    </head>
+
+
+    <body class="fixed-left">
+
+        <!-- Begin page -->
+        <div id="wrapper">
+
+            <!-- Top Bar Start -->
+           <?php include('includes/header.php');?>
+            <!-- ========== Left Sidebar Start ========== -->
+             <?php include('includes/sidebar.php');?>
+            <!-- Left Sidebar End -->
+
+
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container">
+
+
+                        <div class="row">
+							<div class="col-xs-12">
+								<div class="page-title-box">
+                                    <h4 class="page-title">Add Tag </h4>
+                                    <ol class="breadcrumb p-0 m-0">
+                                        <li>
+                                            <a href="#">Tag</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Add Tag </a>
+                                        </li>
+                                        <li class="active">
+                                            Add Tag
+                                        </li>
+                                    </ol>
+                                    <div class="clearfix"></div>
+                                </div>
+							</div>
+						</div>
+                        <!-- end row -->
+
+<div class="row">
+    <div class="col-sm-6">  
+        <!---Success Message--->  
+        <?php if($msg){ ?>
+        <div class="alert alert-success" role="alert">
+        <strong>Well done!</strong> <?php echo htmlentities($msg);?>
+        </div>
+        <?php } ?>
+
+        <!---Error Message--->
+        <?php if($error){ ?>
+            <div class="alert alert-danger" role="alert">
+            <strong>Oh snap!</strong> <?php echo htmlentities($error);?></div>
+        <?php } ?>
+
+    </div>
+</div>
+
+
+                        <div class="row">
+                            <div class="col-md-10 col-md-offset-1">
+                                <div class="p-6">
+                                    <div class="">
+<form action = "add-tag.php" method="POST" enctype="multipart/form-data">
+    <div class="form-group m-b-20">
+        <label for="exampleInputEmail1">Name</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Enter Tag name" required>
+    </div>
+
+
+    <button type="submit" name="submit" class="btn btn-success waves-effect waves-light">Save and Post</button>
+    <button type="button" class="btn btn-danger waves-effect waves-light">Discard</button>
+
+
+                                        </form> <!-- end form -->
+                                    </div>
+                                </div> <!-- end p-20 -->
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+
+
+
+                    </div> <!-- container -->
+
+                </div> <!-- content -->
+
+           <?php include('includes/footer.php');?>
+
+            </div>
+
+
+            <!-- ============================================================== -->
+            <!-- End Right content here -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+        <script src="../plugins/switchery/switchery.min.js"></script>
+
+        <!--Summernote js-->
+        <script src="../plugins/summernote/summernote.min.js"></script>
+        <!-- Select 2 -->
+        <script src="../plugins/select2/js/select2.min.js"></script>
+        <!-- Jquery filer js -->
+        <script src="../plugins/jquery.filer/js/jquery.filer.min.js"></script>
+
+        <!-- page specific js -->
+        <script src="assets/pages/jquery.blog-add.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+        <script>
+
+            jQuery(document).ready(function(){
+
+                $('.summernote').summernote({
+                    height: 240,                 // set editor height
+                    minHeight: null,             // set minimum height of editor
+                    maxHeight: null,             // set maximum height of editor
+                    focus: false                 // set focus to editable area after initializing summernote
+                });
+                // Select2
+                $(".select2").select2();
+
+                $(".select2-limiting").select2({
+                    maximumSelectionLength: 2
+                });
+            });
+        </script>
+  <script src="../plugins/switchery/switchery.min.js"></script>
+
+        <!--Summernote js-->
+        <script src="../plugins/summernote/summernote.min.js"></script>
+
+    </body>
+</html>
+
+<?php } ?>
--- admin/manage-categories.php
+++ admin/manage-categories.php
@@ -1,1 +1,209 @@
-<!-- TO-DO -->
+<?php
+
+include '../consume.php';
+
+session_start();
+error_reporting(0);
+if(strlen($_SESSION['login'])==0)
+  { 
+    session_destroy();
+    header('location:index.php');
+}
+else{
+
+    // Delete cate
+    if($_GET['action']='del'){
+
+        // Get ID of the cate
+        $id = intval($_GET['id']);
+        // Resource Address
+        $url = "http://localhost:8088/myNews/api/category/delete.php?id=$id";
+
+        $data = consume($url);
+
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <!-- App title -->
+        <title>AZ News | Manage Articles</title>
+
+        <!--Morris Chart CSS -->
+		<link rel="stylesheet" href="../plugins/morris/morris.css">
+
+        <!-- jvectormap -->
+        <link href="../plugins/jvectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
+
+        <script src="assets/js/modernizr.min.js"></script>
+
+    </head>
+
+
+    <body class="fixed-left">
+
+        <!-- Begin page -->
+        <div id="wrapper">
+
+            <!-- Top Bar Start -->
+           <?php include('includes/header.php');?>
+
+            <!-- ========== Left Sidebar Start ========== -->
+           <?php include('includes/sidebar.php');?>
+
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container">
+
+
+                        <div class="row">
+							<div class="col-xs-12">
+								<div class="page-title-box">
+                                    <h4 class="page-title">Manage Categories </h4>
+                                    <ol class="breadcrumb p-0 m-0">
+                                        <li>
+                                            <a href="#">Admin</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Categories</a>
+                                        </li>
+                                        <li class="active">
+                                            Manage Categories  
+                                        </li>
+                                    </ol>
+                                    <div class="clearfix"></div>
+                                </div>
+							</div>
+						</div>
+                        <!-- end row -->
+
+
+
+
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card-box">
+                         
+
+                                    <div class="table-responsive">
+                                        <table class="table table-colored table-centered table-inverse m-0">
+                                            <thead>
+
+                                                <tr>                                      
+                                                <th>Name</th>
+                                                </tr>
+
+                                        </thead>
+                                        <tbody>
+
+                                            <?php 
+
+                                            // Get all Categories
+                                            $url = "http://localhost:8088/myNews/api/category/read.php";
+                                            $data = consume($url);
+                                            
+                                            for($i = 0; $i < count($data); $i++){ ?>
+
+                                            <tr>
+                                                <td><?php echo htmlentities($data[$i]['name']);?></td>
+                                                
+
+                                                <td><a href="edit-article.php?id=<?php echo htmlentities($data[$i]['id']);?>">
+                                                <i class="fa fa-pencil" style="color: #29b6f6;"></i></a> 
+                                                &nbsp;<a href="manage-categories.php?id=<?php echo htmlentities($data[$i]['id']);?>&&action=del" onclick="return confirm('Do you really want to delete ?')"> 
+                                                <i class="fa fa-trash-o" style="color: #f05050;"></i></a> </td>
+                                            </tr>
+
+                                            <?php } ?>
+                                               
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div> <!-- container -->
+
+                </div> <!-- content -->
+
+                    <?php include('includes/footer.php');?>
+
+            </div>
+
+
+            <!-- ============================================================== -->
+            <!-- End Right content here -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+        <script src="../plugins/switchery/switchery.min.js"></script>
+
+        <!-- CounterUp  -->
+        <script src="../plugins/waypoints/jquery.waypoints.min.js"></script>
+        <script src="../plugins/counterup/jquery.counterup.min.js"></script>
+
+        <!--Morris Chart-->
+		<script src="../plugins/morris/morris.min.js"></script>
+		<script src="../plugins/raphael/raphael-min.js"></script>
+
+        <!-- Load page level scripts-->
+        <script src="../plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
+        <script src="../plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+        <script src="../plugins/jvectormap/gdp-data.js"></script>
+        <script src="../plugins/jvectormap/jquery-jvectormap-us-aea-en.js"></script>
+
+
+        <!-- Dashboard Init js -->
+		<script src="assets/pages/jquery.blog-dashboard.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+    </body>
+</html>
+
+<?php } ?>
